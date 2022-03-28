9ebf0fee640d85b4afba0f43f429796508618829
YaEtoTui <102538132+YaEtoTui@users.noreply.github.com>
2022-03-28 22:18:12 +0500

    Game
    сделал через рекурсию цикличность, но не знаю как сделать, чтобы через какое-то количество ходов, объект остановился(1-6)

--- Game IT-enterprise/Assets/Scripts/BasicСonstants.cs
+++ Game IT-enterprise/Assets/Scripts/BasicСonstants.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class BasicСonstants : MonoBehaviour
 {
+    public static Dictionary<int, Dictionary<string, Vector3>> dictObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dictObject = new Dictionary<int, Dictionary<string, Vector3>>();
+        dictObject.Add(0, new Dictionary<string, Vector3>());
+        dictObject[0].Add("red", new Vector3(0, 0, Time.deltaTime));
+
+        dictObject.Add(1, new Dictionary<string, Vector3>());
+        dictObject[1].Add("red", new Vector3(4, 0, Time.deltaTime));
+
+        dictObject.Add(2, new Dictionary<string, Vector3>());
+        dictObject[2].Add("red", new Vector3(4, -4, Time.deltaTime));
+
+        dictObject.Add(3, new Dictionary<string, Vector3>());
+        dictObject[3].Add("red", new Vector3(0, -4, Time.deltaTime));
     }
 
     // Update is called once per frame
--- Game IT-enterprise/Assets/Scripts/MoveObject.cs
+++ Game IT-enterprise/Assets/Scripts/MoveObject.cs
@@ -7,21 +7,21 @@ public class MoveObject : MonoBehaviour
     //Наш объект.
     public GameObject Circle;
 
+    //хотел хранить координаты через словарь, отсылаясь на другой скрипт
+    //public Dictionary<int, Dictionary<string, Vector3>> dictObject = BasicСonstants.dictObject;
+
     //Количество ходов.
-    private int NumbersOfMoves;
+    public int NumbersOfMoves;
 
     private int ActualMove;
 
-    private int CurrentCell = -1;
+    private int CurrentCell = 0;
 
-    //Метод который будет выполнятся по нажатию нашей кнопки.
+    //Метод который будет выполнятся по нажатию нашей кнопки.Я делал через рекурсию
     public void OnButtonDown()
     {
-        //текущая ячейка(от 0 -> макс 3(по логике макс 4)
-        if (CurrentCell != 3)
-            CurrentCell++;
-        else
-            CurrentCell = 0;
+        //Рандомные ходы от 1 до 6
+        NumbersOfMoves = Random.Range(1, 6);//надо как-то сделать, чтобы по нажатию генерировалось число
 
         /*по нажатию кнопки пермещается по координатам ниже с интервалом _ или _ секунды(недопонял)
          * */
@@ -31,23 +31,30 @@ public class MoveObject : MonoBehaviour
             Circle.transform.position = new Vector3(4, -4, Time.deltaTime);
         else if (CurrentCell == 2)
             Circle.transform.position = new Vector3(0, -4, Time.deltaTime);
-        else if (CurrentCell == 3)
+        else
             Circle.transform.position = new Vector3(0, 0, Time.deltaTime);
 
+        //Circle.transform.position = dictObject[CurrentCell]["red"];//вот так думал получится, но нет(
 
-        //if (NumbersOfMoves != ActualMove)
-        //{
-        //    ActualMove++;
-        InvokeRepeating("OnButtonDown", 4, 2);
-    //}
-    //    else
-    //        ActualMove = 0;
+        //текущая ячейка(от 0 -> макс 3(по логике макс 4)
+        if (CurrentCell == 3)
+            CurrentCell = 0;
+        else
+            CurrentCell++;
+
+        //тут планировалось сделать рекурсию
+        //вместе с номером кубика, но не получилось
+        if (NumbersOfMoves != ActualMove)
+        {
+            ActualMove++;
+            Invoke("OnButtonDown", 1);
+        }
+        else
+            ActualMove = 0;
     }
 
     public void Start()
     {
-        //Рандомные ходы от 1 до 6
-        NumbersOfMoves = Random.Range(1, 6);
     }
 
     public void Update()
